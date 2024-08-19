53f12472d26efbfea2f71e38d115d054bee35b96
Shanazar <snizamaddinow@gmail.com>
2024-08-19 23:25:39 +0300

    add max subarray sum problem brute force solution.

---
+++ arrays/max_subarray_sum.py
@@ -0,0 +1,36 @@
+import unittest
+
+class Solution(object):
+    def brute_force(self, nums):
+        max_sum = nums[0]
+
+        for i in range(0, len(nums)):
+            cur_sum = 0
+            for j in range(i, len(nums)):
+                cur_sum += nums[j]
+                max_sum = max(max_sum, cur_sum)
+
+        return max_sum
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+        self.test_values = [
+            {'input': [-2,1,-3,4,-1,2,1,-5,4], 'expected': 6},
+            {'input': [1], 'expected': 1},
+            {'input': [5,4,-1,7,8], 'expected': 23}
+        ]
+
+    def run_test(self, method):
+        for test_value in self.test_values:
+            with self.subTest(test=test_value):
+                output = method(test_value['input'])
+                self.assertEqual(output, test_value['expected'])
+
+    def test_brute_force(self):
+        self.run_test(self.solution.brute_force)
+
+
+if __name__ == '__main__':
+    unittest.main()
